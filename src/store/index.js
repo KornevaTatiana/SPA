6305d2010b856ba0f992b2f591e4e9ba00e0679f
Tatiana <tatian4.korneva@yandex.ru>
2020-10-13 15:23:48 +0300

    add commit

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {    
+    newTodos: JSON.parse(localStorage.getItem('newTodos') || '[]')
+  },
+  mutations: {
+    createTodo(state, newTodo){
+      state.newTodos.push(newTodo)
+      localStorage.setItem('newTodos', JSON.stringify(state.newTodos))
+    }
+  },
+  actions: {
+    createTodo({commit}, newTodo){
+      commit('createTodo', newTodo)
+    }
+  },
+  getters:{
+    newTodos: s => s.newTodos       
+  },
+  modules: {
+  }
+})
